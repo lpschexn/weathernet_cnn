108394da6d22e1c4ebc79c682017727fcd948cc0
Logan Schexnaydre <lpschexn@mtu.edu>
2023-02-25 19:30:40 -0500

    Updated stats and test to handle making stats for the entire test set.

--- stats.py
+++ stats.py
@@ -2,90 +2,119 @@ import torch
 
 # Reference formulas https://towardsdatascience.com/precision-and-recall-88a3776c8007
 
-def calc_total_correct(outputs, targets):
-    pass
+class WNStatTracker:
+    def __init__(self):
+        self.cumulative_correct         = 0
+        self.cumulative_false_positives = 0
+        self.cumulative_true_positives  = 0
+        self.cumulative_true_sum        = 0
+        self.cumulative_false_sum       = 0
 
-def calc_total_false_positives(outputs, targets):
-    pass
+    # Input: tensor structure of CNN's output or skitti answers
+    # Output: Indices showing no snow/snow estimate or answer (indices are 0 or 1)
+    def get_label_values(self, input):
+        return torch.argmax(input, 1)
 
-def calc_total_true_positives(outputs, targets):
-    pass
+    def calc_total_correct(self, outputs, targets):
+        
+        output_guesses = self.get_label_values(outputs)
+        target_answers = self.get_label_values(targets)
 
-def calc_true_sum(targets):
-    pass
+        # AND operation to find all instances where the guess matches the answer.
+        # Results in array of ints where 1s are where snow points are matched by the CNN and the label
+        correct_guesses = torch.logical_and(output_guesses, target_answers).int()
 
-def calc_false_sum(targets):
-    pass
+        # Sum up correct guesses into one integer value
+        return torch.sum(correct_guesses).int()
 
-def calc_true_positive_rate(outputs, targets):
-    # Calculate True Positive Rate (a.k.a. Recall)
-    # Inputs:
-    #   - outputs: tensor structure output from CNN
-    #   - targets: tensor structure with skitti answers
-    # Outputs:
-    #   - true positive rate (ratio of correctly guessed true positives and anything that should have been positive)
+    def calc_total_false_positives(self, outputs, targets):
+        output_guesses = self.get_label_values(outputs)
+        target_answers = self.get_label_values(targets)
 
-    # Get the indices that show the best guess of no snow/snow (indices 0 and 1)
-    output_guesses = torch.argmax(outputs, 1)
-    target_answers = torch.argmax(targets, 1)
+        # Identify only cases where output is positive and true answer is false (output_guesses AND NOT target_answers)
+        not_target_answers = torch.logical_not(target_answers).int()
+        false_positives = torch.logical_and(output_guesses, not_target_answers).int()
 
-    # AND operation to find all instances where the guess matches the answer.
-    # Results in array of ints where 1s are where snow points are matched by the CNN and the label
-    correct_guesses = torch.logical_and(output_guesses, target_answers).int()
+        # Sum up false positives
+        return torch.sum(false_positives).int()
 
-    # Sum up correct guesses into one integer value
-    total_correct = torch.sum(correct_guesses)
+    def calc_total_true_positives(self, outputs, targets):
+        output_guesses = self.get_label_values(outputs)
+        target_answers = self.get_label_values(targets)
 
-    # Sum up target answer to get total number of true positives
-    true_sum = torch.sum(target_answers)
+        # Find True Positives (output_guesses AND target_answers)
+        true_positives = torch.logical_and(output_guesses, target_answers).int()
 
-    return total_correct/true_sum
+        return torch.sum(true_positives).int()
 
-def calc_false_positive_rate(outputs, targets):
-    # Calculate False Positive Rate
-    # Inputs:
-    #   - outputs: tensor structure output from CNN
-    #   - targets: tensor structure with skitti answers
-    # Outputs:
-    #   - false positive rate
+    def calc_true_sum(self, targets):
+        target_answers = self.get_label_values(targets)
+        return torch.sum(target_answers).int()
 
-    # Get the indices that shows the best guess of no snow/snow (arrays of indices 0 and 1)
-    output_guesses = torch.argmax(outputs, 1)
-    target_answers = torch.argmax(targets, 1)
+    def calc_false_sum(self, targets):
+        target_answers = self.get_label_values(targets)
+        not_target_answers = torch.logical_not(target_answers).int()
+        return torch.sum(not_target_answers)
 
-    # Identify only cases where output is positive and true answer is false (output_guesses AND NOT target_answers)
-    not_target_answers = torch.logical_not(target_answers).int()
-    false_positives = torch.logical_and(output_guesses, not_target_answers).int()
+    def calc_true_positive_rate(self, outputs, targets):
+        # Calculate True Positive Rate (a.k.a. Recall)
+        # Inputs:
+        #   - outputs: tensor structure output from CNN
+        #   - targets: tensor structure with skitti answers
+        # Outputs:
+        #   - true positive rate (ratio of correctly guessed true positives and anything that should have been positive)
 
-    # Sum up false positives
-    total_false_positives = torch.sum(false_positives)
+        # Get total number of correct values in this batch
+        total_correct = self.calc_total_correct(outputs, targets)
 
-    # Get total negatives
-    false_sum = torch.sum(not_target_answers)
+        # Sum up target answer to get total number of true positives
+        true_sum = self.calc_true_sum(targets)
 
-    return total_false_positives/(false_sum)
+        # Update count for all inputs
+        self.cumulative_correct += total_correct
+        self.cumulative_true_sum += true_sum
 
-def calc_precision(outputs, targets):
-    # Calculate Precision
-    # Inputs:
-    #   - outputs: tensor structure output from CNN
-    #   - targets: tensor structure with skitti answers
-    # Outputs:
-    #   - precision (ratio of true positives and anything guessed positive)
+        return total_correct/true_sum
 
-    # Get the indices that show the best guess of no snow/snow (indices 0 and 1)
-    output_guesses = torch.argmax(outputs, 1)
-    target_answers = torch.argmax(targets, 1)
+    def calc_false_positive_rate(self, outputs, targets):
+        # Calculate False Positive Rate
+        # Inputs:
+        #   - outputs: tensor structure output from CNN
+        #   - targets: tensor structure with skitti answers
+        # Outputs:
+        #   - false positive rate
 
-    # Find True Positives (output_guesses AND target_answers)
-    true_positives = torch.logical_and(output_guesses, target_answers).int()
+        total_false_positives = self.calc_total_false_positives(outputs, targets)
 
-    # Find False Positives (output_guesses AND NOT target_answers)
-    not_target_answers = torch.logical_not(target_answers).int()
-    false_positives = torch.logical_and(output_guesses, not_target_answers).int()
+        false_sum = self.calc_false_sum(targets)
 
-    # Sum up correct guesses into one integer value
-    total_true_positives = torch.sum(true_positives)
-    total_false_positives = torch.sum(false_positives)
+        # Update count for all inputs
+        self.cumulative_false_positives += total_false_positives
+        self.cumulative_false_sum       += false_sum
 
-    return total_true_positives/(total_true_positives + total_false_positives)
+        return total_false_positives/(false_sum)
+
+    def calc_precision(self, outputs, targets):
+        # Calculate Precision
+        # Inputs:
+        #   - outputs: tensor structure output from CNN
+        #   - targets: tensor structure with skitti answers
+        # Outputs:
+        #   - precision (ratio of true positives and anything guessed positive)
+
+        total_true_positives  = self.calc_total_true_positives(outputs, targets)
+        total_false_positives = self.calc_total_false_positives(outputs, targets)
+
+        # Update count for true positives. Update false positives with false positive rate
+        self.cumulative_true_positives  += total_true_positives
+
+        return total_true_positives/(total_true_positives + total_false_positives)
+    
+    def calc_cumulative_true_positive_rate(self):
+        return self.cumulative_correct/self.cumulative_true_sum
+
+    def calc_cumulative_false_positive_rate(self):
+        return self.cumulative_false_positives/(self.cumulative_false_sum)
+
+    def calc_cumulative_precision(self):
+        return self.cumulative_true_positives/(self.cumulative_true_positives + self.cumulative_false_positives)
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch
 from torch.utils.data import DataLoader
 from dataset import WADSDataset
-from stats import calc_true_positive_rate, calc_precision, calc_false_positive_rate
+from stats import WNStatTracker
 
 PATH = 'models/wn_0.pth'
 
@@ -16,18 +16,26 @@ model.load_state_dict(torch.load(PATH))
 dataset = WADSDataset('res/test')
 data_loader = DataLoader(dataset, batch_size=1, shuffle=None)
 
+# Stat Tracker
+stat_tracker = WNStatTracker()
+
+total_tests = 0
+
 # Testing Loop
 with torch.no_grad():
     for i, data in enumerate(data_loader, 0):
+
+        total_tests += 1
+
         # Get inputs and labels
         inputs, targets = data
 
         outputs = model(inputs)
 
         # Print precision, recall, and false positive rates
-        precision = calc_precision(outputs, targets)
-        recall = calc_true_positive_rate(outputs, targets)
-        false_positives = calc_false_positive_rate(outputs, targets)
+        precision = stat_tracker.calc_precision(outputs, targets)
+        recall = stat_tracker.calc_true_positive_rate(outputs, targets)
+        false_positives = stat_tracker.calc_false_positive_rate(outputs, targets)
 
         print(f'===============================================')
         print(f'Batch Number:                 {i}')
@@ -35,4 +43,15 @@ with torch.no_grad():
         print(f'True Positive Rate (Recall):  {recall}')
         print(f'False Positive Rate:          {false_positives}')
 
+print('===============================================')
 print('*Oven Timer Ding* Finished Testing')
+
+cumulative_precision       = stat_tracker.calc_cumulative_precision()
+cumulative_recall          = stat_tracker.calc_cumulative_true_positive_rate()
+cumulative_false_positives = stat_tracker.calc_cumulative_false_positive_rate()
+
+print(f'Cumulative Test Results')
+print(f'Total Number of Tests:        {total_tests}')
+print(f'Precision:                    {cumulative_precision}')
+print(f'True Positive Rate (Recall):  {cumulative_recall}')
+print(f'False Positive Rate:          {cumulative_false_positives}')
